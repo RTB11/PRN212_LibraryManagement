3ed5f34eb56f6fe71afd2c695a1f26f8130189ae
Nguyễn Bá Tú <tbbb0612@gmail.com>
2026-07-08 17:58:49 +0700

    Add Author Page For Admin Role

--- LibraryManagementProject/Views/AuthorPage.xaml.cs
+++ LibraryManagementProject/Views/AuthorPage.xaml.cs
@@ -1,27 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LibraryManagementProject.Model;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace LibraryManagementProject.Views
 {
-    /// <summary>
-    /// Interaction logic for AuthorPage.xaml
-    /// </summary>
-    public partial class AuthorPage : Window
+    public partial class AuthorPage : Page
     {
+
+        private readonly LibraryContext _context = new();
+
+        private Author selectedAuthor;
+
         public AuthorPage()
         {
             InitializeComponent();
+
+            LoadAuthors();
         }
+
+        private void LoadAuthors()
+        {
+            lvAuthors.ItemsSource =
+                _context.Authors.ToList();
+        }
+        private void lvAuthors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedAuthor =
+                lvAuthors.SelectedItem as Author;
+
+
+            if (selectedAuthor != null)
+            {
+                txtAuthorName.Text =
+                    selectedAuthor.AuthorName;
+
+
+                txtBiography.Text =
+                    selectedAuthor.Biography;
+            }
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtAuthorName.Text))
+            {
+                MessageBox.Show("Author name is required");
+                return;
+            }
+
+
+            Author author = new Author();
+
+            author.AuthorName = txtAuthorName.Text;
+
+            author.Biography = txtBiography.Text;
+
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+            MessageBox.Show("Add successfully");
+
+            LoadAuthors();
+            ClearForm();
+
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (selectedAuthor == null)
+            {
+                MessageBox.Show("Please select author");
+                return;
+            }
+
+            selectedAuthor.AuthorName =
+                txtAuthorName.Text;
+            selectedAuthor.Biography =
+                txtBiography.Text;
+
+            _context.SaveChanges();
+            MessageBox.Show("Update successfully");
+            LoadAuthors();
+
+        }
+
+        private void ClearForm()
+        {
+            selectedAuthor = null;
+
+            txtAuthorName.Clear();
+            txtBiography.Clear();
+
+        }
+
     }
 }
